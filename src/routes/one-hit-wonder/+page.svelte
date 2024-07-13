6c7251dc18549ed9a2c54f253204115424512fa7
Daniel Subiabre <subiabrewd@gmail.com>
2024-07-13 13:02:56 +0200

    Cleane one-hit-wonder code

--- src/routes/one-hit-wonder/+page.svelte
+++ src/routes/one-hit-wonder/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { lastfm, type Artist, type Track } from "$lib/lastfm";
+    import { calcPlaycount } from "$lib/stats";
 
     let artistName: string;
     let topTrackCount: string;
@@ -18,21 +19,9 @@
         );
 
         topTracksPercentage = calcPlaycountPercentage(
-            calcTracksPlaycount(topTracks),
+            calcPlaycount(topTracks),
             artist.playcount,
         );
-    }
-
-    function calcTracksPlaycount(tracks: Track[]) {
-        let playcount = 0;
-
-        for (let index = 0; index < tracks.length; index++) {
-            const track = tracks[index];
-
-            playcount = playcount + track.playcount;
-        }
-
-        return playcount;
     }
 
     function calcPlaycountPercentage(playcount: number, total: number) {
@@ -41,16 +30,20 @@
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
-    <input type="text" bind:value={artistName} />
+    <input type="text" placeholder="Artist name" bind:value={artistName} />
     <select bind:value={topTrackCount}>
-        <option value="5" selected>5</option>
-        <option value="10">10</option>
-        <option value="20">20</option>
-        <option value="30">30</option>
+        <option value="5" selected>Top 5 tracks</option>
+        <option value="10">Top 10 tracks</option>
+        <option value="20">Top 20 tracks</option>
+        <option value="30">Top 30 tracks</option>
+        <option value="30">Top 50 tracks</option>
     </select>
     <button type="submit">Submit</button>
 </form>
 
 {#if artist}
-    <p>{topTracksPercentage}% of {artist.name} listens are in the top {topTracks?.length} tracks.</p>
+    <p>
+        {topTracksPercentage}% of {artist.name} listens are in their top {topTracks?.length}
+        tracks.
+    </p>
 {/if}
