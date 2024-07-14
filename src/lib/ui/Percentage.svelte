f5834068944ded6c0f37d4ef4bcd3543311622d6
Daniel Subiabre <subiabrewd@gmail.com>
2024-07-14 21:45:33 +0200

    Add generic percentage component

---
+++ src/lib/ui/Percentage.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let part: number;
+    export let total: number;
+
+    function fmt(v: number) {
+        const formatter = new Intl.NumberFormat(navigator.language ?? "en-US");
+
+        return formatter.format(v);
+    }
+
+    function calcPercentage(part: number, total: number) {
+        return ((part * 100) / total).toFixed(2);
+    }
+</script>
+
+<span title="{fmt(part)} out of {fmt(total)}">
+    {calcPercentage(part, total)}%
+</span>
