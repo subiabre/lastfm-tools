799122ebe6cb1f32e8100ba67e5f5bdf005197d3
Daniel Subiabre <subiabrewd@gmail.com>
2024-12-10 23:17:05 +0100

    Add big number component

---
+++ src/lib/ui/BigNumber.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let number: number;
+
+    function fmt(v: number) {
+        const formatter = new Intl.NumberFormat(navigator.language ?? "en-US");
+
+        return formatter.format(v);
+    }
+</script>
+
+<span title={number.toString()}>
+    {fmt(number)}
+</span>
