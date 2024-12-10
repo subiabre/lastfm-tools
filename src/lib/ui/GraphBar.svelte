a18e7c2f009546515bec11c894442b898f6793d1
Daniel Subiabre <subiabrewd@gmail.com>
2024-12-10 22:12:48 +0100

    Generic graph bar

---
+++ src/lib/ui/GraphBar.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    export let size: number;
+    export let title: string = "";
+
+    const c0020 = "#648fff80";
+    const c2040 = "#785ef080";
+    const c4060 = "#dc267f80";
+    const c6080 = "#fe610080";
+    const c8000 = "#ffb00080";
+
+    let color: string = getColor();
+
+    function getColor() {
+        if (size > 80) {
+            return c8000;
+        }
+
+        if (size > 60) {
+            return c6080;
+        }
+
+        if (size > 40) {
+            return c4060;
+        }
+
+        if (size > 20) {
+            return c2040;
+        }
+
+        return c0020;
+    }
+</script>
+
+<div {title} style="--bar-size: {size}%; --bar-color: {color}">
+    <slot />
+</div>
+
+<style>
+    div {
+        width: 100%;
+
+        padding: 0.25rem;
+
+        background-image: linear-gradient(
+            to right,
+            var(--bar-color) var(--bar-size),
+            rgba(0, 0, 0, 0) var(--bar-size)
+        );
+    }
+</style>
--- src/routes/one-hit-wonder/TrackBar.svelte
+++ src/routes/one-hit-wonder/TrackBar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import GraphBar from "$lib/ui/GraphBar.svelte";
     import Percentage from "$lib/ui/Percentage.svelte";
 
     export let part: number;
@@ -6,53 +7,13 @@
 
     export let text: string;
 
-    let barSize: number = calcBarSize();
+    let size: number = calcBarSize();
     function calcBarSize() {
         return Math.round((part * 100) / total);
     }
-
-    const c0020 = "#648fff80";
-    const c2040 = "#785ef080";
-    const c4060 = "#dc267f80";
-    const c6080 = "#fe610080";
-    const c8000 = "#ffb00080";
-
-    let barColor: string = getBarColor();
-    function getBarColor() {
-        if (barSize > 80) {
-            return c8000;
-        }
-
-        if (barSize > 60) {
-            return c6080;
-        }
-
-        if (barSize > 40) {
-            return c4060;
-        }
-
-        if (barSize > 20) {
-            return c2040;
-        }
-
-        return c0020;
-    }
 </script>
 
-<div style="--bar-size: {barSize}%; --bar-color: {barColor}">
-    <Percentage {total} {part} /> {text}
-</div>
-
-<style>
-    div {
-        width: 100%;
-
-        padding: 0.25rem;
-
-        background-image: linear-gradient(
-            to right,
-            var(--bar-color) var(--bar-size),
-            rgba(0, 0, 0, 0) var(--bar-size)
-        );
-    }
-</style>
+<GraphBar {size}>
+    <Percentage {total} {part} />
+    {text}
+</GraphBar>
